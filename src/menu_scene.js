e058df2fee618111eb46ac1a0eb000f7913dab56
Alex <ajmarksboy@gmail.com>
2018-07-21 17:54:06 -0500

    Movement keys added to start menu

--- src/menu_scene.js
+++ src/menu_scene.js
@@ -12,11 +12,18 @@ module.exports.MenuScene = class MenuScene extends Phaser.Scene {
   }
 
   create() {
-    this.startBtn = this.add.text(400, 300, "start", {
+    this.add.text(300, 200, "Controls \n WASD: Movement \n F: Alien transform \n Mouse1: Shoot acid", {
       font: "18px Arial",
       fill: "#ff0000",
       align: "center",
     })
+    
+    this.startBtn = this.add.text(350, 300, "START", {
+      font: "18px Arial",
+      fill: "#ff0000",
+      align: "center",
+    })
+
       .setInteractive();
     this.startBtn.on('pointerdown', this.start, this);
     this.startBtn.on('pointerover', () => this.startBtn.setFill('#ffff00'), this);
